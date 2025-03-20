50c175340383242d8992f33c14ee98ee112f4776
Верховный Маг <verhovnyjmag@MacBook-Air-od-Verhovnyj.local>
2025-03-20 03:04:13 +0300

    Remove unnecessary files

--- FinanceLibrary.Analytics/IFinancialAnalytics.cs
+++
@@ -1,7 +0,0 @@
-public interface IFinancialAnalytics
-{
-    decimal CalculateBalanceForPeriod(DateTime start, DateTime end);
-    IDictionary<Category, decimal> GroupExpensesByCategory(DateTime start, DateTime end);
-    IDictionary<Category, decimal> GroupIncomeByCategory(DateTime start, DateTime end);
-    // Дополнительная аналитика...
-} 
